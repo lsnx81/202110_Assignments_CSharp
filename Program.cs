57d1eae0fd3cd4d7f9eb6127f0fb22a49488cfb1
stijn <stijn@DESKTOP-HF3QT8Q>
2021-10-19 10:36:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _202110_Assignments_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
